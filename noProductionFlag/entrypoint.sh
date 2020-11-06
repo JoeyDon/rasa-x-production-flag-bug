e315545ec44c4cc11f5c9b616662ecd44a81a6c7
Joey Don <Joey.don@cmd.com.au>
2020-11-06 13:50:01 +1100

    After some debug

--- noProductionFlag/entrypoint.sh
+++ noProductionFlag/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export RASA_X_USERNAME="admin"
+# export RASA_X_USERNAME="admin"
 
-rasa x --no-prompt --enable-api --rasa-x-port 8080 -vv
+# rasa x --no-prompt --enable-api --rasa-x-port 8080 -vv
+rasa x --no-prompt --production --enable-api --rasa-x-port 8080 -vv -e DB_HOST=JOEY
+
+# rasa x --no-prompt
